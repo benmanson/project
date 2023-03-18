f7f21b93ed592e679a498b7ea20b1073a60e4db8
benmanson <ben@benmanson.uk>
2023-03-18 21:10:20 +0000

    Run pip install in script on reload and create default superuser

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+pip install -q -r requirements.txt
+
 while ! nc -z db 3306; do
     echo "MySQL is unavailable - sleeping"
     sleep 2
@@ -7,6 +9,10 @@ done
 echo "MySQL is up - executing commands...."
 python manage.py makemigrations
 python manage.py migrate
+
+echo "Creating default superuser...."
+echo "from django.contrib.auth import get_user_model;get_user_model().objects.create_superuser(username='admin', password='pass');" | python manage.py shell 2> /dev/null
+
 python manage.py collectstatic --no-input
 
 gunicorn -c /usr/src/app/config/dev.py
