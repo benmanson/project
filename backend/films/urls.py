024bd4a91aa44c5a05f2372c78ea5a89b5627e0a
benmanson <benjmanson87@gmail.com>
2023-05-11 11:27:23 +0100

    Remove old apps

--- backend/films/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.decorators import login_required
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("home/", views.index, name="films_home"),
-]
--- backend/frontend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="homepage")
-]
