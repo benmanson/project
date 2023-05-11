d62379e677c2cf9a3f20bcffa0a276b16f9bb085
benmanson <benjmanson87@gmail.com>
2023-05-11 11:27:03 +0100

    Download bootstrap and jquery in docker-entrypoint.sh

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-pip install -q -r requirements.txt
+pip install --no-cache-dir -q -r requirements.txt
+
+if [ ! -f "/var/www/localhost/static/js/jquery.3.6.4.min.js" ]; then
+    wget https://github.com/twbs/bootstrap/releases/download/v5.3.0-alpha1/bootstrap-5.3.0-alpha1-dist.zip -O /var/www/localhost/static/bootstrap-5.3.0-alpha1-dist.zip
+    unzip -o /var/www/localhost/static/bootstrap-5.3.0-alpha1-dist.zip -d /var/www/localhost/static
+    mv /var/www/localhost/static/bootstrap-5.3.0-alpha1-dist/* /var/www/localhost/static
+    rm -rf /var/www/localhost/static/bootstrap-5.3.0-alpha1-dist*
+    wget https://code.jquery.com/jquery-3.6.4.min.js -O /var/www/localhost/static/js/jquery.3.6.4.min.js
+fi
 
 while ! nc -z db 3306; do
     echo "MySQL is unavailable - sleeping"
